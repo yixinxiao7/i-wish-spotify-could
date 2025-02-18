97bb05def5c9aae4348ef33deff93396d8e3b4da
Yixin Xiao <yixinxiao7@gmail.com>
2025-02-17 22:49:09 -0500

    make redirect safer

--- ui/src/app/login/page.tsx
+++ ui/src/app/login/page.tsx
@@ -8,11 +8,18 @@ const client_id = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID
 const redirect_url = String(REDIRECT_URL)
 const scopes_url_params = SCOPES.join("%20")
 
+const generateRandomString = (length = 16) => {
+    const array = new Uint8Array(length);
+    crypto.getRandomValues(array);
+    return Array.from(array, byte => byte.toString(36).padStart(2, '0')).join('').slice(0, length);
+}
+
 const Login: React.FC = () => {
     const handleLogin = () => {
         const authUrl = `${AUTHORIZE_ENDPOINT}?client_id=${client_id}` +
                         `&response_type=code` +
                         `&redirect_uri=${encodeURIComponent(redirect_url)}` +
+                        `&state=${generateRandomString()}` +
                         `&scope=${scopes_url_params}`;
         window.location.href = authUrl;
     }
