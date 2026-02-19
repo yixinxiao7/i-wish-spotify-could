517c15192001a6fd3304090132f5b971f8e46b02
Yixin Xiao <yixinxiao7@gmail.com>
2026-02-19 01:45:35 -0500

    update config

--- ui/src/utils/config.ts
+++ ui/src/utils/config.ts
@@ -15,10 +15,10 @@ export const AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
 export const REDIRECT_URL = `${process.env.NEXT_PUBLIC_WEB_HOST}/callback`;
 
 // Backend APIs
-export const POST_TOKEN_ENDPOINT = `${process.env.NEXT_PUBLIC_SERVER_HOST}/api/oauth`;
-export const GET_TOTAL_SONGS_ENDPOINT = `${process.env.NEXT_PUBLIC_SERVER_HOST}/api/songs/total`; 
-export const GET_SONGS_ENDPOINT = `${process.env.NEXT_PUBLIC_SERVER_HOST}/api/songs`;
-export const GET_PLAYLISTS_ENDPOINT = `${process.env.NEXT_PUBLIC_SERVER_HOST}/api/playlists`;
+export const POST_TOKEN_ENDPOINT = `${process.env.NEXT_PUBLIC_SERVER_HOST}/api/oauth/`;
+export const GET_TOTAL_SONGS_ENDPOINT = `${process.env.NEXT_PUBLIC_SERVER_HOST}/api/songs/total`;
+export const GET_SONGS_ENDPOINT = `${process.env.NEXT_PUBLIC_SERVER_HOST}/api/songs/`;
+export const GET_PLAYLISTS_ENDPOINT = `${process.env.NEXT_PUBLIC_SERVER_HOST}/api/playlists/`;
 export const POST_PLAYLISTS_ADD_SONG_ENDPOINT = `${process.env.NEXT_PUBLIC_SERVER_HOST}/api/playlists/add-song`;
 export const PUT_START_PLAYBACK_ENDPOINT = `${process.env.NEXT_PUBLIC_SERVER_HOST}/api/playback/start`;
 export const PUT_STOP_PLAYBACK_ENDPOINT = `${process.env.NEXT_PUBLIC_SERVER_HOST}/api/playback/stop`;
